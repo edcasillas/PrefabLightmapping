e581d3a8d3a069a84313a5bcb72cb57df1862d47
Ed Casillas <eduardo_casillas@hotmail.com>
2020-01-16 17:09:16 -0600

    Modified to not do baking, so it can use existing lightmaps and just calculate the data. Also tweaked so it won't fail in builds where UnityEditor is not available.

--- Runtime/PrefabLightmapData.cs
+++ Runtime/PrefabLightmapData.cs
@@ -1,25 +1,20 @@
 using UnityEngine;
-using UnityEditor;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
-
-public class PrefabLightmapData : MonoBehaviour
-{
+public class PrefabLightmapData : MonoBehaviour {
     [System.Serializable]
-    struct RendererInfo
-    {
+    struct RendererInfo {
         public Renderer renderer;
-        public int lightmapIndex;
-        public Vector4 lightmapOffsetScale;
+        public int      lightmapIndex;
+        public Vector4  lightmapOffsetScale;
     }
+
     [System.Serializable]
-    struct LightInfo
-    {
+    struct LightInfo {
         public Light light;
-        public int lightmapBaketype;
-        public int mixedLightingMode;
+        public int   lightmapBaketype;
+        public int   mixedLightingMode;
     }
 
     [SerializeField]
@@ -33,11 +28,7 @@ public class PrefabLightmapData : MonoBehaviour
     [SerializeField]
     LightInfo[] m_LightInfo;
 
-
-    void Awake()
-    {
-        Init();
-    }
+    void Awake() { Init(); }
 
     void Init()
     {
@@ -91,26 +82,13 @@ public class PrefabLightmapData : MonoBehaviour
         LightmapSettings.lightmaps = combinedLightmaps2;
     }
 
-    void OnEnable()
-    {
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-
-    }
+    void OnEnable() { SceneManager.sceneLoaded += OnSceneLoaded; }
 
     // called second
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        Init();
-    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode) { Init(); }
 
     // called when the game is terminated
-    void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-
+    void OnDisable() { SceneManager.sceneLoaded -= OnSceneLoaded; }
 
     static void ApplyRendererInfo(RendererInfo[] infos, int[] lightmapOffsetIndex, LightInfo[] lightsInfo)
     {
@@ -146,113 +124,155 @@ public class PrefabLightmapData : MonoBehaviour
     }
 
 #if UNITY_EDITOR
-    [UnityEditor.MenuItem("Assets/Bake Prefab Lightmaps")]
-    static void GenerateLightmapInfo()
-    {
-        if (UnityEditor.Lightmapping.giWorkflowMode != UnityEditor.Lightmapping.GIWorkflowMode.OnDemand)
-        {
+    [UnityEditor.MenuItem("Tools/Bake Prefab Lightmaps", false, 70)]
+    static void GenerateLightmapInfo() {
+        if (UnityEditor.Lightmapping.isRunning) {
+            Debug.LogError($"Please wait until lightmap baking finishes!");
+            return;
+        }
+        if (UnityEditor.Lightmapping.giWorkflowMode != UnityEditor.Lightmapping.GIWorkflowMode.OnDemand) {
             Debug.LogError("ExtractLightmapData requires that you have baked you lightmaps and Auto mode is disabled.");
             return;
         }
-        UnityEditor.Lightmapping.Bake();
+        
+        //UnityEditor.EditorUtility.DisplayProgressBar("Baking Prefab Lightmaps", "Baking lightmaps", 0);
 
+        //UnityEditor.Lightmapping.completed += OnBakeCompleted;
+
+        /*if (!UnityEditor.Lightmapping.BakeAsync()) {
+            UnityEditor.Lightmapping.completed -= OnBakeCompleted;
+            Debug.LogError($"Baking could not be started.");
+        } else {
+            Debug.Log($"Baking has started.");
+        }*/
+        
+        OnBakeCompleted();
+    }
+
+    private static void OnBakeCompleted() {
+        //UnityEditor.Lightmapping.completed -= OnBakeCompleted;
         PrefabLightmapData[] prefabs = FindObjectsOfType<PrefabLightmapData>();
-
-        foreach (var instance in prefabs)
-        {
-            var gameObject = instance.gameObject;
+        //Debug.Log(prefabs.Length);
+        //UnityEditor.EditorUtility.ClearProgressBar();
+        
+        foreach (var instance in prefabs) {
+            var gameObject    = instance.gameObject;
             var rendererInfos = new List<RendererInfo>();
-            var lightmaps = new List<Texture2D>();
-            var lightmapsDir = new List<Texture2D>();
-            var shadowMasks = new List<Texture2D>();
-            var lightsInfos = new List<LightInfo>();
+            var lightmaps     = new List<Texture2D>();
+            var lightmapsDir  = new List<Texture2D>();
+            var shadowMasks   = new List<Texture2D>();
+            var lightsInfos   = new List<LightInfo>();
 
             GenerateLightmapInfo(gameObject, rendererInfos, lightmaps, lightmapsDir, shadowMasks, lightsInfos);
 
             instance.m_RendererInfo = rendererInfos.ToArray();
-            instance.m_Lightmaps = lightmaps.ToArray();
+            instance.m_Lightmaps    = lightmaps.ToArray();
             instance.m_LightmapsDir = lightmapsDir.ToArray();
-            instance.m_LightInfo = lightsInfos.ToArray();
-            instance.m_ShadowMasks = shadowMasks.ToArray();
+            instance.m_LightInfo    = lightsInfos.ToArray();
+            instance.m_ShadowMasks  = shadowMasks.ToArray();
 
-            var targetPrefab = PrefabUtility.GetCorrespondingObjectFromOriginalSource(instance.gameObject) as GameObject;
-            if (targetPrefab != null)
-            {
-                GameObject root = PrefabUtility.GetOutermostPrefabInstanceRoot(instance.gameObject);// 根结点
+            var targetPrefab =
+                UnityEditor.PrefabUtility.GetCorrespondingObjectFromOriginalSource(instance.gameObject) as GameObject;
+            if (targetPrefab != null) {
+                GameObject root = UnityEditor.PrefabUtility.GetOutermostPrefabInstanceRoot(instance.gameObject); // 根结点
+
                 //如果当前预制体是是某个嵌套预制体的一部分（IsPartOfPrefabInstance）
-                if (root != null)
-                {
-                    GameObject rootPrefab = PrefabUtility.GetCorrespondingObjectFromSource(instance.gameObject);
-                    string rootPath = AssetDatabase.GetAssetPath(rootPrefab);
+                if (root != null) {
+                    GameObject rootPrefab =
+                        UnityEditor.PrefabUtility.GetCorrespondingObjectFromSource(instance.gameObject);
+                    string rootPath = UnityEditor.AssetDatabase.GetAssetPath(rootPrefab);
+
                     //打开根部预制体
-                    PrefabUtility.UnpackPrefabInstanceAndReturnNewOutermostRoots(root, PrefabUnpackMode.OutermostRoot);
-                    try
-                    {
+                    UnityEditor.PrefabUtility.UnpackPrefabInstanceAndReturnNewOutermostRoots(root,
+                                                                                             UnityEditor
+                                                                                                 .PrefabUnpackMode
+                                                                                                 .OutermostRoot);
+                    try {
                         //Apply各个子预制体的改变
-                        PrefabUtility.ApplyPrefabInstance(instance.gameObject, InteractionMode.AutomatedAction);
-                    }
-                    catch { }
-                    finally
-                    {
+                        UnityEditor.PrefabUtility.ApplyPrefabInstance(instance.gameObject,
+                                                                      UnityEditor.InteractionMode.AutomatedAction);
+                    } catch { } finally {
                         //重新更新根预制体
-                        PrefabUtility.SaveAsPrefabAssetAndConnect(root, rootPath, InteractionMode.AutomatedAction);
+                        UnityEditor.PrefabUtility.SaveAsPrefabAssetAndConnect(root,
+                                                                              rootPath,
+                                                                              UnityEditor.InteractionMode
+                                                                                         .AutomatedAction);
                     }
-                }
-                else
-                {
-                    PrefabUtility.ApplyPrefabInstance(instance.gameObject, InteractionMode.AutomatedAction);
+                } else {
+                    UnityEditor.PrefabUtility.ApplyPrefabInstance(instance.gameObject,
+                                                                  UnityEditor.InteractionMode.AutomatedAction);
                 }
             }
         }
-
-
     }
 
-    static void GenerateLightmapInfo(GameObject root, List<RendererInfo> rendererInfos, List<Texture2D> lightmaps, List<Texture2D> lightmapsDir, List<Texture2D> shadowMasks, List<LightInfo> lightsInfo)
-    {
+    private static void GenerateLightmapInfo(GameObject      root,
+                                             List<RendererInfo> rendererInfos,
+                                             List<Texture2D> lightmaps,
+                                             List<Texture2D> lightmapsDir, 
+                                             List<Texture2D> shadowMasks,
+                                             List<LightInfo> lightsInfo) {
+        UnityEditor.EditorUtility.DisplayProgressBar($"Baking Prefab Lightmaps for {root.name}", $"Generating LightmapInfo for {root.name}", 0);
+
+        var renderersWithNoLightmapIndex = 0;
         var renderers = root.GetComponentsInChildren<MeshRenderer>();
-        foreach (MeshRenderer renderer in renderers)
-        {
-            if (renderer.lightmapIndex != -1)
-            {
-                RendererInfo info = new RendererInfo();
-                info.renderer = renderer;
+        for (var i = 0; i < renderers.Length; i++) {
+            var renderer = renderers[i];
+            UnityEditor.EditorUtility.DisplayProgressBar($"Baking Prefab Lightmaps for {root.name}",
+                                                         $"Settings textures for {renderer.name}",
+                                                         (i / (float) renderers.Length) / 2);
+            
+            var lightmapIndex = renderer.lightmapIndex;
 
-                if (renderer.lightmapScaleOffset != Vector4.zero)
-                {
-                    info.lightmapOffsetScale = renderer.lightmapScaleOffset;
+            if (lightmapIndex == -1) {
+                renderersWithNoLightmapIndex++;
+                continue;
+            }
 
-                    Texture2D lightmap = LightmapSettings.lightmaps[renderer.lightmapIndex].lightmapColor;
-                    Texture2D lightmapDir = LightmapSettings.lightmaps[renderer.lightmapIndex].lightmapDir;
-                    Texture2D shadowMask = LightmapSettings.lightmaps[renderer.lightmapIndex].shadowMask;
+            if (lightmapIndex >= LightmapSettings.lightmaps.Length) {
+                Debug.LogError($"A renderer in {renderer.gameObject.name} has a lightmapIndex out of the range of LightmapSettings.lightmaps");
+                continue;
+            }
+            
+            var info = new RendererInfo {renderer = renderer};
 
-                    info.lightmapIndex = lightmaps.IndexOf(lightmap);
-                    if (info.lightmapIndex == -1)
-                    {
-                        info.lightmapIndex = lightmaps.Count;
-                        lightmaps.Add(lightmap);
-                        lightmapsDir.Add(lightmapDir);
-                        shadowMasks.Add(shadowMask);
-                    }
+            if (renderer.lightmapScaleOffset != Vector4.zero) {
+                info.lightmapOffsetScale = renderer.lightmapScaleOffset;
 
-                    rendererInfos.Add(info);
+                Texture2D lightmap    = LightmapSettings.lightmaps[lightmapIndex].lightmapColor;
+                Texture2D lightmapDir = LightmapSettings.lightmaps[lightmapIndex].lightmapDir;
+                Texture2D shadowMask  = LightmapSettings.lightmaps[lightmapIndex].shadowMask;
+
+                info.lightmapIndex = lightmaps.IndexOf(lightmap);
+                if (info.lightmapIndex == -1) {
+                    info.lightmapIndex = lightmaps.Count;
+                    lightmaps.Add(lightmap);
+                    lightmapsDir.Add(lightmapDir);
+                    shadowMasks.Add(shadowMask);
                 }
-                    
+
+                rendererInfos.Add(info);
             }
         }
+        
+        Debug.LogWarning($"Renderers with no lightmap index: {renderersWithNoLightmapIndex} out of {renderers.Length}");
 
         var lights = root.GetComponentsInChildren<Light>(true);
 
-        foreach (Light l in lights)
-        {
-            LightInfo lightInfo = new LightInfo();
-            lightInfo.light = l;
-            lightInfo.lightmapBaketype = (int)l.lightmapBakeType;
-            lightInfo.mixedLightingMode = (int)UnityEditor.LightmapEditorSettings.mixedBakeMode;
-            lightsInfo.Add(lightInfo);
+        for (var i = 0; i < lights.Length; i++) {
+            Light l = lights[i];
+            UnityEditor.EditorUtility.DisplayProgressBar($"Baking Prefab Lightmaps for {root.name}",
+                                                         $"Processing light {l.name}",
+                                                         .5f + ((i / (float) lights.Length) / 2));
 
+            LightInfo lightInfo = new LightInfo();
+            lightInfo.light             = l;
+            lightInfo.lightmapBaketype  = (int) l.lightmapBakeType;
+            lightInfo.mixedLightingMode = (int) UnityEditor.LightmapEditorSettings.mixedBakeMode;
+            lightsInfo.Add(lightInfo);
         }
+        
+        UnityEditor.EditorUtility.ClearProgressBar();
     }
 #endif
-
 }
